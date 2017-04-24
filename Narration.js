74547cd8dbd29d67593e76f7d50add75cb712f1f
Mehak Shamdasani <mshamdasani@gmail.com>
2017-04-23 22:08:41 -0400

    Add files via upload
    Narration for levels

---
+++ Narration.js
@@ -0,0 +1,10 @@
+//Level1
+responsiveVoice.speak("You are now on the first floor of South Building. The Chancellor’s office is located on the second floor of the building. There is a guard patrolling the hallway in front of the stairs. You need to reach the staircase without being spotted by the guard. When you think the guard has walked past you, press spacebar to run across the hallway.", "US English Female");
+//Level2
+responsiveVoice.speak("You will begin in the safe zone. A guard is patrolling the waiting area in front of the Chancellors office. It is your job to knock the guard out with your baton before you can enter the office. To move forward press spacebar and use the left or right arrow key to throw your baton in the direction of the guard.", "US English Female");
+//Level3
+responsiveVoice.speak("Now, inside of Chancellor Folt’s office, the keys to end the madness is in her safe.", "US English Female");
+//If these were intentionally split up use
+//responsiveVoice.pause();
+//responsiveVoice.resume();
+responsiveVoice.speak("Her 2-number combination can save the city. Use arrows to dial in a particular direction. Doorbell sound will indicate the correct number. Spacebar to save that number. Best of luck.", "US English Female");
